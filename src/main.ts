37c3e74154e6d95f7120abde518730fb1e2e0a70
dongsipan <75098051@qq.com>
2020-03-16 11:12:30 +0800

    init project

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 全局引入按需引入UI库 vant
+import '@/plugins/vant'
+// 移动端适配
+import 'lib-flexible/flexible.js'
+
 Vue.config.productionTip = false
 
 new Vue({
